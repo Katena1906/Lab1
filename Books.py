ae75aa83c9760e21bed97ca822aebfa0faeac8d7
Katena <katuskamakarona60@gmail.com>
2024-11-20 11:02:38 +0300

    add cr for book

--- Books.py
+++ Books.py
@@ -1,4 +1,10 @@
 from Author import Author
 class Book:
     def __init__(self, title:str, author:Author,isbn:int, price:float, genre:str )-> None:
+        self.title: str = title
+        self.author: Author = author
+        self.isbn: int = isbn
+        self.price: float = price
+        self.genre: str =genre
+
 
