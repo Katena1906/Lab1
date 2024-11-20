66977583f7dc31b34044e8eb4e492a7240183090
Katena <katuskamakarona60@gmail.com>
2024-11-20 17:20:54 +0300

    some

--- ShopPlatform.py
+++ ShopPlatform.py
@@ -18,3 +18,27 @@ class ShopPlatform:
             raise TypeError("Expected book instance")
         self.books.append(book)
         print(f"{book.title} added")
+
+    def remove_user(self, user: User) -> None:
+        if not isinstance(user, User):
+           raise TypeError("Expected user instance")
+        try:
+            self.users.remove(user)
+            print(f"{user.username} removed")
+        except ValueError:
+            print(f"'{user.username} not in platform")
+
+    def remove_book(self, book: Book) -> None:
+        if not isinstance(book, Book):
+            raise TypeError("Expected Book instance")
+        try:
+            self.books.remove(book)
+            print(f"'{book.title}' removed from platform")
+        except ValueError:
+            print(f"'{book.title}' not in platform")
+
+    def find_book(self,title: str) -> None:
+        for book in self.books:
+            if title==book.title:
+                print(f"Book {title} find")
+
