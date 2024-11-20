5e4c09d3a21bbb9c3071ec27bce92fac0be1abec
Katena <katuskamakarona60@gmail.com>
2024-11-20 12:06:21 +0300

    del author in book

--- Books.py
+++ Books.py
@@ -1,16 +1,11 @@
-from Author import Author
+
 class Book:
-    def __init__(self, title:str, author:Author,isbn:int, price:float, genre:str )-> None:
+    def __init__(self, title:str,isbn:int, price:float, genre:str )-> None:
         self.title: str = title
-        self.author: Author = author
         self.isbn: int = isbn
         self.price: float = price
         self.genre: str =genre
 
     def info(self) -> None:
         print(
-            f"Title: {self.title}\nAuthor: {self.author.name}\nISBN: {self.isbn}\nPrice: {self.price}\nGenre: {self.genre}")
-
-
-
-
+            f"Title: {self.title}\nISBN: {self.isbn}\nPrice: {self.price}\nGenre: {self.genre}")
