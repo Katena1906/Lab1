1a797b4a083769b76343a145a0328e71ec16d0af
Katena <katuskamakarona60@gmail.com>
2024-11-20 12:28:28 +0300

    defs for user

--- Users.py
+++ Users.py
@@ -6,3 +6,16 @@ class User:
         self.username: str = username
         self.email: str = email
         self.purchased: List[Book] = []
+
+    def purchase(self, book: Book) -> None:
+        self.purchased.append(book)
+        print(f"{self.username} purchased '{book.title}'.")
+
+    def list_purchased_books(self) -> None:
+        if not self.purchased:
+            print(f"{self.username} has not purchased any books.")
+            return
+        print(f"{self.username}'s purchased books:")
+        for book in self.purchased:
+            print(f"- {book.title} by {book.author.name} {book.author.lastname}")
+
