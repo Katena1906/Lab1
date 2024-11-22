c9cf1399ea5fa28b714bf15d68e033e95d7d33e1
Katena <katuskamakarona60@gmail.com>
2024-11-22 05:17:10 +0300

    exp for book author

--- Author.py
+++ Author.py
@@ -1,6 +1,10 @@
 
 class Author:
     def __init__(self, name: str, lastname: str):
+        if not isinstance(name, str):
+            raise TypeError("Name must be string")
+        if not isinstance(lastname, str):
+            raise TypeError("Lastname must be string")
         self.name: str = name
         self.lastname: str = lastname
 
--- Books.py
+++ Books.py
@@ -1,6 +1,16 @@
 from Author import Author
 class Book:
     def __init__(self, title: str,isbn: int, price: float, genre: str, author: Author)-> None:
+        if not isinstance(title, str):
+            raise TypeError("Title must be a string.")
+        if not isinstance(isbn, int):
+            raise TypeError("ISBN must be an integer.")
+        if not isinstance(price, (float, int)) or price < 0:
+            raise ValueError("Price must be positive number")
+        if not isinstance(genre, str):
+            raise TypeError("Genre must be string")
+        if not isinstance(author, Author):
+            raise TypeError("Author must be instance of Author class")
         self.title: str = title
         self.isbn: int = isbn
         self.price: float = price
